65123fdd72da997074020d59781d6be9fa4f9ad1
chamoli11 <chamolianshul1311@gmail.com>
2021-10-11 23:25:57 +0530

    secondtask

---
+++ anshul/Task4.java
@@ -0,0 +1,14 @@
+public class Task4 {
+    public static void main(String[] args) {
+        
+        for ( int i =0; i<=5; i++){
+            System.out.print("\n");
+            for (int n=5; n>i;n--)
+            {
+                System.out.print("* ");
+            }
+
+        }
+    }
+    
+}
