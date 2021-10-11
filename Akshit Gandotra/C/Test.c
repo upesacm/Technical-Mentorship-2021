5fa7c0537725670e4cebda3b9de6e7ee7620d397
akshit-g <gandotra.akshit@gmail.com>
2021-10-11 23:07:53 +0530

    c program added

---
+++ Akshit Gandotra/C/Test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*#include<conio.h>*/
+#include<math.h>
+
+
+void main(){
+
+/**********************************
+*Author: Chandan S
+*Purpose: Demo and Introduction
+**********************************/
+//USE FUL COMMAND TO EXECUTE THE CODE
+/*EXECUTE THE CODE*/
+/*gcc -o HelloWorld HelloWorld.c -lm (BUILD PHASE)
+./HelloWorld (RUN) */
+
+
+	printf("Hello!! My name is Akshit\n");
+}
